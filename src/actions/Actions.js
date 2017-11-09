91a62efe0c29c0cd139581fb3468690e5eea282f
FreemenL <liuxiajiang818@163.com>
2017-11-09 18:41:40 +0800

    Update Actions.js

--- src/actions/Actions.js
+++ src/actions/Actions.js
@@ -17,7 +17,13 @@ export const setVisibilityFilter = (filter)=>({
 })
 
 
+/*
+    Action 是一个对象。其中的type属性是必须的，表示 Action 的名称,reducer便是根据type属性来执行相应的操作 其他属性可以自由设置。
 
+    redux的核心 就是说用户不能直接去操作store 必须由action来完成这个任务     流程就是由view触发action 再由action去 操作store  当store改变时
+
+    就会触发view 的从新渲染 
+*/ 
 
 
 
