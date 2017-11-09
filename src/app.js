42dffe115a386588b9c2a660d93b68e64b6b75fa
FreemenL <liuxiajiang818@163.com>
2017-11-09 18:37:38 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,11 @@ import todoApp from 'reducers/index';
 import App from 'components/App.js';
 
 const store = createStore(todoApp);
+/*
+  Provider 相当于一个最顶层的组件  通过store属性 把createStore创建的store 置为整个应用的最顶层状态
 
+  然后在connect中通过this.context 来获取到这个最顶层的 状态传给 子孙组件(在这里值connect()()的第二个单数传进去的组件)；
+*/ 
 
 render(
     <Provider store={store}>
